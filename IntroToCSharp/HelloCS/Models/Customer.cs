e369c31b61e9b344574bc99d27d6e4d69e5c320a
Otuonyeadike Chisom <cotuonyeadike@gmail.com>
2024-01-11 14:24:34 +0100

    First Commit

---
+++ IntroToCSharp/HelloCS/Models/Customer.cs
@@ -0,0 +1,30 @@
+﻿namespace HelloCS;
+
+public class Customer
+    {
+        #region Properties
+    public string FirstName {get; private set;}
+    public string LastName {get; private set;}
+    public string FullName{
+    get => Firstname + " " LastName;}
+    public string Email {get; private set;}
+    #endregion
+    
+    #region Methods
+    public void UpdateInfo(string firstName, string lastName)
+    {
+        if(string.IsNullOrWhiteSpace(firstName) && string.IsNullOrWhiteSpace(lastName)){
+            return;
+        }
+        if(!string.IsNullOrWhiteSpace(firstName) && !string.IsNullOrWhiteSpace(lastName)){
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        else if(!string.IsNullOrWhiteSpace(firstName)){
+            FirstName = firstName;
+        }
+        else if(!string.IsNullOrWhiteSpace(lastName)){
+            LastName = lastName;
+        }
+    }
+}
